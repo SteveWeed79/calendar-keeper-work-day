a28819e8bfbabd9d5671109e55d541eed8007790
unknown <stevewee1979@gmail.com>
2022-07-15 11:54:58 -0500

    Got the buttons and text field pulling.  Need to get them into an array in local storage now.

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,40 +1,45 @@
 var now = moment().format("h A");
-var timeNow = moment().format("dddd, MMMM Do H:MM A");
-const nineAM = document.getElementById("nineAM").textContent;
+var timeNow = moment().format("dddd, MMMM Do @ H:MM A");
 const timeHero = document.getElementById("currentDay");
 const textAreaToColor = document.getElementById("nineAMtext")
 var timeChange = moment().format("H A");
 var testOne = document.getElementsByClassName("input-group-text").textContent;
+var dayPlan = [];
 
 
 $(document).ready(function () {
-    $("button").click(function () {
-        alert("The button worked!");
-        alert($("#nineAMtest").text());
+    $("button").on("click", function () {
+        var value = $(this)
+        .siblings('.textarea')
+        .val()
+        console.log(value)
+
+        var timeId = $(this)
+        .siblings('.textarea')
+        .attr('id');
+        
+
+        console.log("the button worked");
+        console.log(value);
+        console.log(timeId);
     });
 });
 
 function printTime() {
     timeHero.textContent = timeNow;
-    console.log(now)
-    console.log(testOne)
-    document.getElementById("nineAM").textContent = now;
-    //document.getElementsByClassName("test").textContent = now;
-
-    if (nineAM === now) {
-        textAreaToColor.setAttribute("class", "present form-control textarea");
-    } else if (nineAM <= now) {
-        textAreaToColor.setAttribute("class", "past form-control textarea");
-    } else {
-        textAreaToColor.setAttribute("class", "future form-control textarea");
-    }
-};
-
-function saveAppointment(event) {
-
 }
 
 
+//if ( testOne === now) {
+   // textAreaToColor.setAttribute("class", "present form-control textarea");
+//} else if ( <= now) {
+    //textAreaToColor.setAttribute("class", "past form-control textarea");
+//} else {
+//    textAreaToColor.setAttribute("class", "future form-control textarea");
+//}
+//};
+
+//function saveAppointment(event) {
 
 
 
