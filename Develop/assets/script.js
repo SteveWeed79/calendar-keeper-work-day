eb9847894a8d4d56d805f8f68beaa9c99153ad97
unknown <steveweed1979@gmail.com>
2022-07-16 13:26:49 -0500

    Added localstorage object.

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -5,25 +5,43 @@ const textAreaToColor = document.getElementById("nineAMtext")
 var timeChange = moment().format("H A");
 var testOne = document.getElementsByClassName("input-group-text").textContent;
 var dayPlan = [];
+var buttonTrue = false
+var localStorageEl = JSON.parse(localStorage.getItem("dayPlan"));
 
 
 $(document).ready(function () {
     $("button").on("click", function () {
         var value = $(this)
-        .siblings('.textarea')
-        .val()
+            .siblings('.textarea')
+            .val()
         console.log(value)
 
+        buttonTrue = true;
+
         var timeId = $(this)
-        .siblings('.textarea')
-        .attr('id');
-        
+            .siblings('.textarea')
+            .attr('id');
+
+        dayPlan.push({
+            timeId,
+            text: value,
+            buttonTrue: buttonTrue,
+        });
+
+        localStorage.setItem("dayPlan", JSON.stringify(dayPlan));
 
         console.log("the button worked");
         console.log(value);
         console.log(timeId);
+        console.log(dayPlan);
+        console.log(localStorageEl);
+        console.log(localStorageEl[buttonTrue]);
+
     });
 });
+
+
+
 
 function printTime() {
     timeHero.textContent = timeNow;
@@ -31,9 +49,9 @@ function printTime() {
 
 
 //if ( testOne === now) {
-   // textAreaToColor.setAttribute("class", "present form-control textarea");
+// textAreaToColor.setAttribute("class", "present form-control textarea");
 //} else if ( <= now) {
-    //textAreaToColor.setAttribute("class", "past form-control textarea");
+//textAreaToColor.setAttribute("class", "past form-control textarea");
 //} else {
 //    textAreaToColor.setAttribute("class", "future form-control textarea");
 //}
