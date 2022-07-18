7860f610a9b7844fe3a599af7b6bb1a618ddd183
unknown <stevewee1979@gmail.com>
2022-07-18 11:44:01 -0500

    Worked on coloring of the time frames and text population.

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,8 +1,6 @@
-var now = moment().format("h A");
+var now = moment().format("H");
 var timeNow = moment().format("dddd, MMMM Do @ H:MM A");
 const timeHero = document.getElementById("currentDay");
-const textAreaToColor = document.getElementById("nineAMtext")
-var timeChange = moment().format("H A");
 var testOne = document.getElementsByClassName("input-group-text").textContent;
 var dayPlan = [];
 var localStorageEl = JSON.parse(localStorage.getItem("dayPlan"));
@@ -11,11 +9,9 @@ var localStorageEl = JSON.parse(localStorage.getItem("dayPlan"));
 
 document.addEventListener('click', function (e) {
     if (e.target.tagName == "BUTTON") {
-        alert('BUTTON CLICKED');
         this.getElementById('hour-9').textContent = localStorageEl[1]
     }
 })
-
 
 
 $(document).ready(function () {
@@ -23,74 +19,45 @@ $(document).ready(function () {
         var value = $(this)
             .siblings('.textarea')
             .val()
-        console.log(value)
-
 
 
         var timeId = $(this)
             .siblings('.textarea')
             .attr('id');
 
-        dayPlan.push({
-            timeId,
-            textEl: value,
-
-        });
-
-        localStorage.setItem("dayPlan", JSON.stringify(dayPlan));
-
-
-
-        console.log("the button worked");
-        console.log(value);
-        console.log(timeId);
-        console.log(dayPlan);
-        console.log(localStorageEl[1]);
-
-
+        localStorage.setItem(timeId, value);
     });
 });
 
-
+$("#hour-9").val(localStorage.getItem("hour-9")); 
+$("#hour-10").val(localStorage.getItem("hour-10")); 
+$("#hour-11").val(localStorage.getItem("hour-11")); 
+$("#hour-12").val(localStorage.getItem("hour-12")); 
+$("#hour-13").val(localStorage.getItem("hour-13")); 
+$("#hour-14").val(localStorage.getItem("hour-14")); 
+$("#hour-15").val(localStorage.getItem("hour-15")); 
+$("#hour-16").val(localStorage.getItem("hour-16")); 
+$("#hour-17").val(localStorage.getItem("hour-17")); 
 
 function printTime() {
     timeHero.textContent = timeNow;
     let testEl = localStorage.getItem("dayPlan");
     let obj = JSON.parse(testEl);
-    document.getElementById('hour-9').textContent = obj.textEl;
-    console.log(timeChange);
+   
 
-    $("#nineAM,#tenAM,#elevenAm,#twelvePM,#onePM,#twoPM,#threePM,#fourPM,#fivePM").each(function () {
-        if (this == now) {
-            document.getElementsByClassName(textarea).setAttribute("class", "present");
-        } else if (this >= now) {
-            document.getElementsByClassName(textarea).setAttribute("class", "future");
+    $(".form-control").each(function () {
+        let hourId = $(this).attr("id");
+        let hour = parseInt(hourId.split("-")[1]);
+
+        if (hour == now) {
+            $(this).addClass("present")
+        } else if (hour >= now) {
+            $(this).addClass("future")
         } else {
-            document.getElementsByClassName(textarea).setAttribute("class", "past");
+            $(this).addClass("past")
         }
-        //$("#hour-9 .description").val(localStorage.getItem("hour-9", "text"));
-
     });
-
 };
-
-
-//if ( testOne === now) {
-// textAreaToColor.setAttribute("class", "present form-control textarea");
-//} else if ( <= now) {
-//textAreaToColor.setAttribute("class", "past form-control textarea");
-//} else {
-//    textAreaToColor.setAttribute("class", "future form-control textarea");
-//}
-//};
-
-//function saveAppointment(event) {
-
-
-
-
-
-
 
 
 
